09bea3a1729f801e36bc2f9d2602119c76dcb513
Jose Pablo Barragan <jpbarragann@gmail.com>
2023-08-10 20:43:17 -0400

    Added status data.

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,12 +3,18 @@ import satData from "./satData";
 const Table = ({ sat }) => {
 
 let dataJSX = sat.map((data, id) => {
+  let isOperational = "";
+  if (data.operational){
+    isOperational = "active";
+  } else {
+    isOperational = "inactive";
+  }
   return (
     <tr key={id}>
         <td>{data.name}</td>
         <td>{data.type}</td>
         <td>{data.launchDate}</td>
-        <td>{data.operational}</td>
+        <td>{isOperational}</td>
       </tr>
   )
 }
